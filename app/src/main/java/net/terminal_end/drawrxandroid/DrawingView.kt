5d8ac8a312128dd812dfc2d1cc716669d86044db
S-Shimotori <490.frostybirds@gmail.com>
2016-05-15 18:00:18 +0900

    Implement drawing with Rx

--- app/src/main/java/net/terminal_end/drawrxandroid/DrawingView.kt
+++ app/src/main/java/net/terminal_end/drawrxandroid/DrawingView.kt
@@ -8,6 +8,7 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import com.jakewharton.rxbinding.view.RxView
 
 /**
  * Created by S-Shimotori on 5/15/16.
@@ -22,25 +23,24 @@ class DrawingView(context: Context, attrs: AttributeSet?): View(context, attrs) 
         paint.style = Paint.Style.STROKE
         paint.isAntiAlias = true
         paint.strokeWidth = 10.0f
+
+        RxView.touches(this).subscribe {
+            val x = it.x
+            val y = it.y
+
+            when(it.action) {
+                MotionEvent.ACTION_DOWN -> path.moveTo(x, y)
+                MotionEvent.ACTION_MOVE -> path.lineTo(x, y)
+                MotionEvent.ACTION_UP -> path.lineTo(x, y)
+            }
+
+            invalidate()
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawPath(path, paint)
-    }
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val x = event.x
-        val y = event.y
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> path.moveTo(x, y)
-            MotionEvent.ACTION_MOVE -> path.lineTo(x, y)
-            MotionEvent.ACTION_UP -> path.lineTo(x, y)
-        }
-
-        invalidate()
-        return true
     }
 
     fun delete() {
